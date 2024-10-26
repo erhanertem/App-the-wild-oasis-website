bac48889e6100741b2fa418f31509c58ce1acce8
Erhan Ertem <eertem@gmail.com>
2024-10-26 13:46:24 +0300

    Replace regular tag elements with performant nextjs counterparts

--- app/components/Logo.js
+++ app/components/Logo.js
@@ -1,12 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
+
 function Logo() {
   return (
-    <a href='/' className='flex items-center gap-4 z-10'>
+    <Link href='/' className='flex items-center gap-4 z-10'>
       {/* src attr uses anything from public folder just like vite */}
-      <img src='/logo.png' height='60' width='60' alt='The Wild Oasis logo' />
+      <Image src='/logo.png' height='60' width='60' alt='The Wild Oasis logo' />
       <span className='text-xl font-semibold text-primary-100'>
         The Wild Oasis
       </span>
-    </a>
+    </Link>
   );
 }
 
