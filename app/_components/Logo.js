b6dccf064bd88497aa688139e17eb90650a3b789
Erhan Ertem <eertem@gmail.com>
2024-10-27 22:58:55 +0300

    Image optimization thru nextjs Image component -external/inline referencing differences

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
+import logo from "@/public/logo.png";
 
 function Logo() {
   return (
     <Link href="/" className="z-10 flex items-center gap-4">
       {/* src attr uses anything from public folder just like vite */}
-      <Image src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" />
+      {/* <Image src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" /> */}
+      <Image src={logo} height="60" width="60" alt="The Wild Oasis logo" />
       <span className="text-xl font-semibold text-primary-100">
         The Wild Oasis
       </span>
