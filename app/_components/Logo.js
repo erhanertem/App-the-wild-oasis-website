097510cbcf905af9b2bbdb5d49a617187c739fe4
Erhan Ertem <eertem@gmail.com>
2024-10-27 23:24:21 +0300

    Explore nextjs Image component api, modify homepage

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -5,9 +5,17 @@ import logo from "@/public/logo.png";
 function Logo() {
   return (
     <Link href="/" className="z-10 flex items-center gap-4">
+      {/* INLINE IMAGE REFERENCING */}
       {/* src attr uses anything from public folder just like vite */}
       {/* <Image src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" /> */}
-      <Image src={logo} height="60" width="60" alt="The Wild Oasis logo" />
+      {/* EXTERNAL IMAGE REFERENCING */}
+      <Image
+        src={logo}
+        quality={100} // This attr defines the quality of image. The lesser quality the more image degradation
+        height="60"
+        width="60"
+        alt="The Wild Oasis logo"
+      />
       <span className="text-xl font-semibold text-primary-100">
         The Wild Oasis
       </span>
--- app/page.js
+++ app/page.js
@@ -1,12 +1,29 @@
+import Image from "next/image";
 import Link from "next/link";
+import bg from "@/public/bg.png";
 
-function Page() {
+export default function Page() {
   return (
-    <div>
-      <h1>The Wild Oasis. Welcome to paradise.</h1>
-      <Link href='/cabins'>Explore luxury cabins</Link>
-    </div>
+    <main className="mt-24">
+      <Image
+        src={bg}
+        fill // Next.js Image component API: stretch image full
+        placeholder="blur" // Next.js Image component API: Image gets blurred till its fully downloaded
+        className="object-cover object-top" // Preserve aspect ratio thru tailwind while the image is forced to fill its container - in order to avoid image jumping around while resizing, we use object-top to begin scaling image from top.
+        alt="Mountains and forests with two cabins"
+      />
+
+      <div className="relative z-10 text-center">
+        <h1 className="mb-10 text-8xl font-normal tracking-tight text-primary-50">
+          Welcome to paradise.
+        </h1>
+        <Link
+          href="/cabins"
+          className="bg-accent-500 px-8 py-6 text-lg font-semibold text-primary-800 transition-all hover:bg-accent-600"
+        >
+          Explore luxury cabins
+        </Link>
+      </div>
+    </main>
   );
 }
-
-export default Page;
