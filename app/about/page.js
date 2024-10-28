4f03c75acd05b2651a5b54faf8ff6b8877dedbe3
Erhan Ertem <eertem@gmail.com>
2024-10-28 13:37:17 +0300

    Edit about page

--- app/about/page.js
+++ app/about/page.js
@@ -1,14 +1,93 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import about1img from "@/public/about-1.jpg";
+
 // OVERRIDE METADATA FROM THE ROOTLAYOUT
 export const metadata = {
   title: "About",
 };
 
-function Page() {
+export default function Page() {
   return (
-    <div>
-      <h1>About the Wild Oasis</h1>
+    <div className="grid grid-cols-5 items-center gap-x-24 gap-y-32 text-lg">
+      <div className="col-span-3">
+        <h1 className="mb-10 text-4xl font-medium text-accent-400">
+          Welcome to The Wild Oasis
+        </h1>
+
+        <div className="space-y-8">
+          <p>
+            Where nature's beauty and comfortable living blend seamlessly.
+            Hidden away in the heart of the Italian Dolomites, this is your
+            paradise away from home. But it's not just about the luxury cabins.
+            It's about the experience of reconnecting with nature and enjoying
+            simple pleasures with family.
+          </p>
+          <p>
+            Our 8 luxury cabins provide a cozy base, but the real freedom and
+            peace you'll find in the surrounding mountains. Wander through lush
+            forests, breathe in the fresh air, and watch the stars twinkle above
+            from the warmth of a campfire or your hot tub.
+          </p>
+          <p>
+            This is where memorable moments are made, surrounded by nature's
+            splendor. It's a place to slow down, relax, and feel the joy of
+            being together in a beautiful setting.
+          </p>
+        </div>
+      </div>
+      <div className="col-span-2">
+        {/* When its possible to reference an image externally and responsive img is desired... */}
+        <Image
+          src={about1img}
+          quality={60}
+          width={20}
+          placeholder="blur"
+          alt="Family sitting around a fire pit in front of cabin"
+        />
+      </div>
+      <div className="relative col-span-2 aspect-square">
+        {/* Lets say its not possible or a URL img reference is desired to import an img, inline is indispensible and if we still need respoinsiveness we would need fill attr */}
+        <Image
+          src="/about-2.jpg"
+          fill
+          className="object-cover"
+          quality={60}
+          alt="Family that manages The Wild Oasis"
+        />
+      </div>
+      <div className="col-span-3">
+        <h1 className="mb-10 text-4xl font-medium text-accent-400">
+          Managed by our family since 1962
+        </h1>
+
+        <div className="space-y-8">
+          <p>
+            Since 1962, The Wild Oasis has been a cherished family-run retreat.
+            Started by our grandparents, this haven has been nurtured with love
+            and care, passing down through our family as a testament to our
+            dedication to creating a warm, welcoming environment.
+          </p>
+          <p>
+            Over the years, we've maintained the essence of The Wild Oasis,
+            blending the timeless beauty of the mountains with the personal
+            touch only a family business can offer. Here, you're not just a
+            guest; you're part of our extended family. So join us at The Wild
+            Oasis soon, where tradition meets tranquility, and every visit is
+            like coming home.
+          </p>
+
+          <div>
+            <Link
+              href="/cabins"
+              className="mt-4 inline-block bg-accent-500 px-8 py-5 text-lg font-semibold text-primary-800 transition-all hover:bg-accent-600"
+            >
+              Explore our luxury cabins
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default Page;
