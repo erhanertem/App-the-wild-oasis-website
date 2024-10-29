240baedbc49d1c43eb909df4b95265e5d60a2b05
Erhan Ertem <eertem@gmail.com>
2024-10-29 17:05:15 +0300

    Generate tab metadata dynamically via generateMetadata() fn

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -2,18 +2,20 @@ import { getCabin } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
-// PLACEHOLDER DATA
-const cabin = {
-  id: 89,
-  name: "001",
-  maxCapacity: 2,
-  regularPrice: 250,
-  discount: 0,
-  description:
-    "Discover the ultimate luxury getaway for couples in the cozy wooden cabin 001. Nestled in a picturesque forest, this stunning cabin offers a secluded and intimate retreat. Inside, enjoy modern high-quality wood interiors, a comfortable seating area, a fireplace and a fully-equipped kitchen. The plush king-size bed, dressed in fine linens guarantees a peaceful nights sleep. Relax in the spa-like shower and unwind on the private deck with hot tub.",
-  image:
-    "https://dclaevazetcjjkrzczpc.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg",
-};
+// OVERRIDE METADATA FROM THE ROOTLAYOUT - STATIC
+// export const metadata = {
+//   title: "Cabin",
+// };
+// OVERRIDE METADATA FROM THE ROOTLAYOUT - DYNAMIC
+export async function generateMetadata({ params }) {
+  const { cabinId } = await params;
+  // console.log(cabinId);
+  const { name } = await getCabin(cabinId);
+  // console.log(name);
+  return {
+    title: `Cabin ${name}`,
+  };
+}
 
 // EACH PAGE UNDER [...] FOLDER, RECEIVES A PARAMS PROP ex: @URL req as cabins/78 for a [cabinId] folder params returns {cabinId=78} object
 export default async function Page({ params }) {
