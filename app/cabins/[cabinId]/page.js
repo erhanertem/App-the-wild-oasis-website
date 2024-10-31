129cb59f6738f7fdc36b96ef274c568bbc77b0ad
Erhan Ertem <eertem@gmail.com>
2024-10-31 23:44:23 +0300

    Turn dynamic params route into static routes with SSG+ISR

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -17,20 +17,24 @@ export async function generateMetadata({ params }) {
   };
 }
 
+// ADOPT ISR ON TOP OF SSG
+export const revalidate = 60;
 /**
  * In Next.js, the generateStaticParams function is used to specify which dynamic routes should be statically generated at build time when using Static Site Generation (SSG). The idsArr array returned by generateStaticParams is a list of objects, each containing parameters (like cabinId) that Next.js will use to pre-render each page specified by those parameters.
 1.	This is especially useful when you have a known set of routes at build time (e.g., specific cabin IDs) and want to generate static pages for each one.
 2.	It optimizes performance because the pages are pre-generated and do not require a server request when accessed.
- */
-// export async function generateStaticParams() {
-//   const cabins = await getCabins();
-//   // console.log(cabins);
-//   const idsArr = cabins.map((cabin) => ({
-//     cabinId: String(cabin.id),
-//   }));
-//   // console.log(idsArr);
-//   return idsArr;
-// }
+*/
+export async function generateStaticParams() {
+  // FETCH THE CABINS
+  const cabins = await getCabins();
+  // console.log(cabins);
+  // RETURN AN ARRAY OF cabinId objects w/ stringified params [{cabinId: '88'}, {cabinId: '123'}, ...]
+  const idsArr = cabins.map((cabin) => ({
+    cabinId: String(cabin.id),
+  }));
+  // console.log(idsArr);
+  return idsArr;
+}
 
 // EACH PAGE UNDER [...] FOLDER, RECEIVES A PARAMS PROP ex: @URL req as cabins/78 for a [cabinId] folder params returns {cabinId=78} object
 export default async function Page({ params }) {
