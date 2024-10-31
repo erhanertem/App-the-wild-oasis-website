e1675a617219b3303f5a6c9c7edaceac9dd6868f
Erhan Ertem <eertem@gmail.com>
2024-11-01 02:20:22 +0300

    Using CC in SC, use 'use client' directive to make component CC, test SC inside a use client boundary turning into CC

---
+++ app/_components/TextExpander.js
@@ -0,0 +1,27 @@
+// MAKES IT CC SO THAT WE CAN USE USESTATE WHICH IS EXCLUSIVE TO CLIENT SIDE REACT
+"use client";
+
+import Logo from "@/app/_components/Logo";
+import { useState } from "react";
+
+function TextExpander({ children }) {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const displayText = isExpanded
+    ? children
+    : children.split(" ").slice(0, 40).join(" ") + "...";
+
+  return (
+    <span>
+      {displayText}{" "}
+      <button
+        className="border-b border-primary-700 pb-1 leading-3 text-primary-700"
+        onClick={() => setIsExpanded(!isExpanded)}
+      >
+        {isExpanded ? "Show less" : "Show more"}
+      </button>
+      <Logo />
+    </span>
+  );
+}
+
+export default TextExpander;
