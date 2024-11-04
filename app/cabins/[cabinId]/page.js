6b0037dfe8be94e81d9ab3a60a90f2742bf15e04
Erhan Ertem <eertem@gmail.com>
2024-11-04 20:04:44 +0300

    Trash unused destructure vars and extract the cluttering code into its own component

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,11 +1,9 @@
 import { Suspense } from "react";
-import Image from "next/image";
-import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 
 import { getCabin, getCabins } from "@/app/_lib/data-service";
 import Reservation from "@/app/_components/Reservation";
-import TextExpander from "@/app/_components/TextExpander";
 import Spinner from "@/app/_components/Spinner";
+import Cabin from "@/app/_components/Cabin";
 
 // OVERRIDE METADATA FROM THE ROOTLAYOUT - STATIC
 // export const metadata = {
@@ -23,7 +21,7 @@ export async function generateMetadata({ params }) {
 }
 
 // ADOPT ISR ON TOP OF SSG
-export const revalidate = 60;
+// export const revalidate = 60;
 /**
  * In Next.js, the generateStaticParams function is used to specify which dynamic routes should be statically generated at build time when using Static Site Generation (SSG). The idsArr array returned by generateStaticParams is a list of objects, each containing parameters (like cabinId) that Next.js will use to pre-render each page specified by those parameters.
 1.	This is especially useful when you have a known set of routes at build time (e.g., specific cabin IDs) and want to generate static pages for each one.
@@ -63,53 +61,12 @@ export default async function Page({ params }) {
   // > ALT#3. ONLY REQUIRED DATA FETCHER IS KEPT. THE REST IS SUB-OUT TO SUB SC.
   const cabin = await getCabin(cabinId);
 
-  const { id, name, maxCapacity, regularPrice, discount, image, description } =
-    cabin;
+  const { name } = cabin;
 
   return (
-    <div className="mx-auto mt-8 max-w-6xl">
-      <div className="mb-24 grid grid-cols-[3fr_4fr] gap-20 border border-primary-800 px-10 py-3">
-        <div className="relative -translate-x-3 scale-[1.15]">
-          <Image
-            src={image}
-            className="object-cover"
-            fill
-            alt={`Cabin ${name}`}
-          />
-        </div>
-
-        <div>
-          <h3 className="mb-5 w-[150%] translate-x-[-254px] bg-primary-950 p-6 pb-1 text-7xl font-black text-accent-100">
-            Cabin {name}
-          </h3>
-
-          <p className="mb-10 text-lg text-primary-300">
-            <TextExpander>{description}</TextExpander>
-          </p>
-
-          <ul className="mb-7 flex flex-col gap-4">
-            <li className="flex items-center gap-3">
-              <UsersIcon className="h-5 w-5 text-primary-600" />
-              <span className="text-lg">
-                For up to <span className="font-bold">{maxCapacity}</span>{" "}
-                guests
-              </span>
-            </li>
-            <li className="flex items-center gap-3">
-              <MapPinIcon className="h-5 w-5 text-primary-600" />
-              <span className="text-lg">
-                Located in the heart of the{" "}
-                <span className="font-bold">Dolomites</span> (Italy)
-              </span>
-            </li>
-            <li className="flex items-center gap-3">
-              <EyeSlashIcon className="h-5 w-5 text-primary-600" />
-              <span className="text-lg">
-                Privacy <span className="font-bold">100%</span> guaranteed
-              </span>
-            </li>
-          </ul>
-        </div>
+    <>
+      <div className="mx-auto mt-8 max-w-6xl">
+        <Cabin cabin={cabin} />
       </div>
 
       <div>
@@ -117,13 +74,12 @@ export default async function Page({ params }) {
           Reserve {name} today. Pay on arrival.
         </h2>
 
-        <p>ERNIE</p>
         {/* USE SUSPENSE MECHANISM TO STOP SC BLOCKING THE ENTIRE PAGE LOADDUE TO ON-GOING FETCHING OPERATIONS INSIDE */}
         <Suspense fallback={<Spinner />}>
           {/* // >#3. CREATE A SUB SC TO HANDLE REST OF FETCHING FOR THE CCs ITS RESPONSIBLE FOR TO MAKE USE OF RPGRESSIVE STREAMING */}
           <Reservation cabin={cabin} />
         </Suspense>
       </div>
-    </div>
+    </>
   );
 }
