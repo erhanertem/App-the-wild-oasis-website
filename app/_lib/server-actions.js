6d903ab17a54b6ee1e9cb9185ab4e33d54777a4b
Erhan Ertem <eertem@gmail.com>
2024-11-13 03:05:58 +0300

    Add zod+conform client+serverside validation

--- app/_lib/server-actions.js
+++ app/_lib/server-actions.js
@@ -1,27 +1,30 @@
 // DIRECTIVE USED FOR FUNCTIONS SERVING SERVER ACTIONS
 "use server";
 
+import { parseWithZod } from "@conform-to/zod";
+
 import { auth, signIn, signOut } from "@/app/_lib/auth";
 import { updateGuest } from "@/app/_lib/data-service";
+import { userFormSchema } from "@/app/_lib/zod-schema";
 
-export async function updateProfile(formData) {
+export async function updateProfile(prevState, formData) {
   // console.log("👉Server action: ", formData);
   // AUTHENTICATION HANDLING: SERVER ACTIONS NEEDS TO BE CHECKED FOR AUTHENTICATION BEFORE PROCEEDING WITH MUTATIONS
   // GUARD CLAUSE - IT'S A COMMON PRACTICE NOT TO USE TRY...CATCH IN SERVER ACTIONS
   // AUTHENTICATION CHECK TO PROCEED WITH REST OF THE ACTIONS
   const session = await auth();
-  // HIT THE SERVER-SIDE CLOSEST ERROR BOUNDARY
-  // ERROR BOUNDARY HANDLING: IN ORDER TO CREATE CUSTOM ERROR BOUNDARY INSTEAD OF A GLOBAL ONE YOU CAN PLACE A NEW ERROR.JS TO THE ROUTE WHERE THIS FUNCTION IS TRIGGERED WHICH IS @ ACCOUNT/PROFILE
+  // // HIT THE SERVER-SIDE CLOSEST ERROR BOUNDARY
+  // // ERROR BOUNDARY HANDLING: IN ORDER TO CREATE CUSTOM ERROR BOUNDARY INSTEAD OF A GLOBAL ONE YOU CAN PLACE A NEW ERROR.JS TO THE ROUTE WHERE THIS FUNCTION IS TRIGGERED WHICH IS @ ACCOUNT/PROFILE
   if (!session) throw new Error("You must be logged in");
+
+  // SETUP CONFORM+ZOD ON THE BACKEND FOR SERVER-SIDE RE-VALIDATION
+  const submission = parseWithZod(formData, { schema: userFormSchema });
+  // CHECK CONFORM FORM SUBMISSION SUCCESS
+  if (submission.status !== "success") return submission.reply();
 
   // READ FORM DATA
   const nationalID = formData.get("nationalID");
   const [nationality, countryFlag] = formData.get("nationality").split("%");
-
-  // CLIENT SIDE ENTRY ERROR TO BE PASSED ONTO CLIENT AS FEEDBACK
-  if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID)) {
-    throw new Error("Please provide a valid national ID");
-  }
 
   const updateData = { nationality, countryFlag, nationalID };
   // console.log(updateData);
---
+++ app/_lib/zod-schema.js
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+// Define a schema for your form
+export const userFormSchema = z.object({
+  nationality: z.string(),
+  nationalID: z.string().regex(/^[a-zA-Z0-9]{6,12}$/, {
+    message: "Please provide a proper national ID",
+  }),
+});
